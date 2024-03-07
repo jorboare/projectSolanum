f81429066cff0f540da244f5fbecaf8dbe2e6e8c
jboronat <jboronat@deloitte.es>
2024-03-07 18:42:17 +0100

    PlanetList Tidy Up

--- src/components/PlanetsList/PlanetsList.tsx
+++ src/components/PlanetsList/PlanetsList.tsx
@@ -18,9 +18,7 @@ interface SatContainerProps {
 const PlanetsList: React.FC = () => {
   const {
     state,
-    cleanState,
     selectPlanet,
-    handleOrbits,
     setMouseInactive,
     selectedPlanets,
     showPlanetList,
@@ -66,30 +64,13 @@ const PlanetsList: React.FC = () => {
 
   //@ts-ignore
   const handleFullScreen = () => {
-    if (
-      document.fullscreenElement ||
-      document.mozFullScreenElement ||
-      document.webkitFullscreenElement ||
-      document.msFullscreenElement
-    ) {
+    if (document.fullscreenElement) {
       if (document.exitFullscreen) {
         document.exitFullscreen();
-      } else if (document.mozCancelFullScreen) {
-        document.mozCancelFullScreen();
-      } else if (document.webkitExitFullscreen) {
-        document.webkitExitFullscreen();
-      } else if (document.msExitFullscreen) {
-        document.msExitFullscreen();
       }
     } else {
       if (document.documentElement.requestFullscreen) {
         document.documentElement.requestFullscreen();
-      } else if (document.documentElement.mozRequestFullScreen) {
-        document.documentElement.mozRequestFullScreen();
-      } else if (document.documentElement.webkitRequestFullscreen) {
-        document.documentElement.webkitRequestFullscreen();
-      } else if (document.documentElement.msRequestFullscreen) {
-        document.documentElement.msRequestFullscreen();
       }
     }
   };
